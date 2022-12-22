d15682e2b588f2814d6d01faa1e47d765b718da0
Mazen421 <81808269+Mazen421@users.noreply.github.com>
2022-12-22 17:50:36 +0200

    Made our library of vectors to use for the program and our own namespace
    since we are not allowed to use vectors we created our own vectors and library with all the functions we want to use, and we created a namespace for it called fywauv which is an acronym

--- Computer programming project.cpp
+++ Computer programming project.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <iostream>
 #include <filesystem>
-#include <vector>
 #include <cstdlib>
 #include <windows.h>
 #include <ctime>
@@ -11,6 +10,153 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+namespace fywauv {
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <cstdlib>
+
+
+
+	//a class that imitates a vector for strings using a dynamic array with push, pop, size, delete clear and print functions because the vector library isn't allowed :/
+	class vector
+	{
+		//class variables
+		std::string* array;
+		int size;
+		int capacity;
+
+	public:
+		//constructor that creates an empty dynamic array of size 0
+		vector()
+		{
+			array = new std::string[0];
+			size = 0;
+			capacity = 0;
+		}
+
+		//push function that adds a new string to the end of the array and doubles the capacity if the array is full to avoid overflow
+		void push(std::string newString)
+		{
+
+
+
+			//if the array is full, create a new array with double the capacity and copy the data from the old array into the new one
+			if (size == capacity)
+			{
+				if (capacity == 0)          //update capacity on first call
+				{
+					capacity = 1;
+				}
+				capacity = capacity * 2;
+				std::string* newArray = new std::string[capacity];
+				for (int i = 0; i < size; i++)
+				{
+					newArray[i] = array[i];
+				}
+				array = newArray;
+			}
+
+			//add the new string to the end of the array and increment the size
+			array[size] = newString;
+			size++;
+
+		}
+
+		//pop function that removes the last string from the array and halves the capacity if the array is 1/4 full
+		void pop()
+		{
+			if (size == 0)
+			{
+				std::cout << "The array is empty. There is nothing to pop." << std::endl;  //if the array is empty, do nothing
+			}
+
+			else
+			{
+				if (size <= capacity / 4)                                    //if the array is 1/4 full (gurantees stuff in it cause we double when we add)
+				{
+					std::string* tempArray = new std::string[size];
+					for (int i = 0; i < size; i++)
+					{
+						tempArray[i] = array[i];
+					}
+					delete[] array;                                           //same as push but in reverse not rewriting all that again, fight me -turnip
+					capacity = capacity / 2;
+					array = new std::string[capacity];
+					for (int i = 0; i < size; i++)
+					{
+						array[i] = tempArray[i];
+					}
+					delete[] tempArray;
+				}
+				size--;
+			}
+		}
+
+		//function that returns the size of the array
+		int getsize()
+		{
+			return size;                     //just return the size we were using
+		}
+
+		//function that deletes the array and creates a new empty array
+		void clear() {
+			delete[] array;
+			array = new std::string[0];                 //remove the array and replace it with an array of size and capacity 0
+			size = 0;
+			capacity = 0;
+		}
+
+		//function that deletes the array at the specified index
+		void erase(int index)
+		{
+			if (index >= size)
+			{
+				std::cout << "The index specified is out of range." << std::endl;
+			}
+			else
+			{
+				for (int i = index; i < size - 1; i++)
+				{
+					array[i] = array[i + 1];
+				}
+				size--;
+			}
+		}
+
+		//function that prints the array
+		void print()
+		{
+			//if the vecotr is empty, print that it is empty
+			if (size == 0)
+			{
+				std::cout << "The array is empty." << std::endl;
+			}
+			//else, print the contents
+			else
+			{
+				for (int i = 0; i < size; i++)
+				{
+					std::cout << array[i] << std::endl;
+				}
+			}
+
+		}
+		//function that returns the string at the specified index
+		std::string get(int index)
+		{
+			if (index >= size)
+			{
+				std::cout << "The index specified is out of range." << std::endl;
+				return "error";
+			}
+			else
+			{
+				return array[index];
+			}
+		}
+	};
+}
 
 
 
@@ -84,7 +230,7 @@ int loginfunction() { //simple login function
 
 }
 
-void listsongs(vector <string>& songs, string path, vector<string>& itemlist) { //this function is very important, it is the function we use to read the files in the folders
+void listsongs(fywauv::vector& songs, string path, fywauv::vector& itemlist) { //this function is very important, it is the function we use to read the files in the folders
 
 
 	//This was made by Ahmed for further questions but I will be writing the commends to explain it here for now -Mazen
@@ -112,8 +258,8 @@ void listsongs(vector <string>& songs, string path, vector<string>& itemlist) { 
 		string ext = item.substr(item.find_last_of(".\\")); //if the file is not a folder it will run this instead which gets the extension of the file
 		if (ext == ".wav" || ext == ".mp3") { //after it gets the "ext" or extension it will see if it mp3 or wav, we can add as many audio types as we want but for now we test with wav and mp3
 			cout << count << "- " << item << endl; //this outputs the names of the items it finds 
-			songs.push_back(entry.path().string()); //this puts the path of the audio fill in the "songs" vector
-			itemlist.push_back(item); //this puts the name of the song into the "itemlist" vector
+			songs.push(entry.path().string()); //this puts the path of the audio fill in the "songs" vector
+			itemlist.push(item); //this puts the name of the song into the "itemlist" vector
 			count++;
 		}
 
@@ -122,7 +268,7 @@ void listsongs(vector <string>& songs, string path, vector<string>& itemlist) { 
 }
 
 
-void listplaylist(vector <string>& playlist, string extpath, int ipath, vector<string>& itemlist) { //I will be using this to controll the playlist
+void listplaylist(fywauv::vector& playlist, string extpath, int ipath, fywauv::vector& itemlist) { //I will be using this to controll the playlist
 
 
 	//This was made by Mazen for further questions
@@ -145,8 +291,8 @@ void listplaylist(vector <string>& playlist, string extpath, int ipath, vector<s
 	bool whilecontroller = true; //this bool value will be used to exist the while loops inside the playlist controller since I am too lazy to figure out how to break it otherwise
 	int switchcontroller;
 	string path;
-	vector<string> controller;
-	vector<string> additemlist;
+	fywauv::vector controller;
+	fywauv::vector additemlist;
 
 
 	/*________________________________The Playlist controller________________________________*/
@@ -188,14 +334,14 @@ void listplaylist(vector <string>& playlist, string extpath, int ipath, vector<s
 
 					string ext = item.substr(item.find_last_of(".\\"));
 					if (ext == ".wav" || ext == ".mp3") {
-						controller.push_back(entry.path().string()); //unlike the list songs function this pushes back to two new vectors called "controller" and "additemlist" which we use as temporary vectors for the path and song names in the folder we are picking a song to add to the playlist from
-						additemlist.push_back(item);
+						controller.push(entry.path().string()); //unlike the list songs function this pushes back to two new vectors called "controller" and "additemlist" which we use as temporary vectors for the path and song names in the folder we are picking a song to add to the playlist from
+						additemlist.push(item);
 
 					}
 				}
 				cout << "current controller playlist" << endl; //shows you the current items in the folder you are picking from
-				for (int i = 0; i < additemlist.size(); i++) {
-					cout << i + 1 << "- " << additemlist[i] << endl;
+				for (int i = 0; i < additemlist.getsize(); i++) {
+					cout << i + 1 << "- " << additemlist.get(i) << endl;
 
 				}
 
@@ -205,20 +351,20 @@ void listplaylist(vector <string>& playlist, string extpath, int ipath, vector<s
 					switch (choice)
 					{
 					case 1: //this function will add the entirity of the folder to the playlist
-						if (controller.size() == 0) {
+						if (controller.getsize() == 0) {
 							cout << "Out of tracks in this folder" << endl; //this is used to prevent the program from adding the songs over and over again
 							whilecontroller = false;
 							break;
 						}
-						for (int i = 0; i < controller.size(); i++) { //this pushes items from the temporary "controller" and "additemlist" vectors to the "playlist" and "additemlist" vectors 
-							playlist.push_back(controller[i]);
-							itemlist.push_back(additemlist[i]);
+						for (int i = 0; i < controller.getsize(); i++) { //this pushes items from the temporary "controller" and "additemlist" vectors to the "playlist" and "additemlist" vectors 
+							playlist.push(controller.get(i));
+							itemlist.push(additemlist.get(i));
 
 						}
 
 						cout << "current controller playlist" << endl; //shows you the new "itemlist" which is the name of the songs in the playlist
-						for (int i = 0; i < itemlist.size(); i++) {
-							cout << i + 1 << "- " << itemlist[i] << endl;
+						for (int i = 0; i < itemlist.getsize(); i++) {
+							cout << i + 1 << "- " << itemlist.get(i) << endl;
 						}
 
 						controller.clear(); //clears the temporary vectors
@@ -227,28 +373,28 @@ void listplaylist(vector <string>& playlist, string extpath, int ipath, vector<s
 						break;
 					case 2: //this will add one song to the list 
 						cout << "Pick a song to add to the playlist" << endl;
-						for (int i = 0; i < additemlist.size(); i++) { //this shows you the song name list of the stuff in the temporay vector
-							cout << i + 1 << "- " << additemlist[i] << endl;
+						for (int i = 0; i < additemlist.getsize(); i++) { //this shows you the song name list of the stuff in the temporay vector
+							cout << i + 1 << "- " << additemlist.get(i) << endl;
 						}
 
-						if (controller.size() == 0) { //this ensure this code stops working as soon as the there are no more songs to add from this list
+						if (controller.getsize() == 0) { //this ensure this code stops working as soon as the there are no more songs to add from this list
 							cout << "Out of tracks in this folder" << endl;
 							whilecontroller = false;
 							break;
 						}
 
-						cin >> itemchoice; 
-						if (itemchoice > controller.size()) { //this ensure the given value is in the vector so we don't get an error
+						cin >> itemchoice;
+						if (itemchoice > controller.getsize()) { //this ensure the given value is in the vector so we don't get an error
 							cout << "item doesn't exist" << endl;
 							whilecontroller = false;
 							break;
 						}
 
-						playlist.push_back(controller[itemchoice - 1]); //this pushes the path of the song from the temp vector to the playlist vector
-						itemlist.push_back(additemlist[itemchoice - 1]); //this pushes the name of the song from the temp vector to the playlist vector
-						cout << "added " << additemlist[itemchoice - 1] << " to the playlist" << endl; //shows you what you picked
-						controller.erase(controller.begin() + itemchoice - 1); //removes the path you picked from the temp list so you can't pick it twice
-						additemlist.erase(additemlist.begin() + itemchoice - 1); //remove the name of the song you picked you picked from the temp list so you can't pick it twice
+						playlist.push(controller.get(itemchoice - 1)); //this pushes the path of the song from the temp vector to the playlist vector
+						itemlist.push(additemlist.get(itemchoice - 1)); //this pushes the name of the song from the temp vector to the playlist vector
+						cout << "added " << additemlist.get(itemchoice - 1) << " to the playlist" << endl; //shows you what you picked
+						controller.erase(itemchoice - 1); //removes the path you picked from the temp list so you can't pick it twice
+						additemlist.erase(itemchoice - 1); //remove the name of the song you picked you picked from the temp list so you can't pick it twice
 
 						break;
 
@@ -268,18 +414,18 @@ void listplaylist(vector <string>& playlist, string extpath, int ipath, vector<s
 			case 3: //remove a song from the playlist
 				count = 1;
 				cout << "current playlist" << endl; //shows you the current playlist
-				for (int i = 0; i < playlist.size(); i++) {
-					cout << i + 1 << "- " << itemlist[i] << endl;
+				for (int i = 0; i < playlist.getsize(); i++) {
+					cout << i + 1 << "- " << itemlist.get(i) << endl;
 				}
 
 				cout << "pick a track to remove" << endl;
 				cin >> choice;
-				itemlist.erase(itemlist.begin() + choice - 1); //remove the path you picked from the playlist
-				playlist.erase(playlist.begin() + choice - 1);//remove the name of the song you picked from the playlist
+				itemlist.erase(choice - 1); //remove the path you picked from the playlist
+				playlist.erase(choice - 1);//remove the name of the song you picked from the playlist
 
 				cout << "current playlist" << endl; //shows you the playlist after the song was removed
-				for (int i = 0; i < itemlist.size(); i++) {
-					cout << i + 1 << "- " << itemlist[i] << endl;
+				for (int i = 0; i < itemlist.getsize(); i++) {
+					cout << i + 1 << "- " << itemlist.get(i) << endl;
 				}
 
 				break;
@@ -304,11 +450,12 @@ string mcicommand(string path) {
 	* but mci send string takes a LPCSTR string as the command for it to work
 	* I had to make this function, which will create the command for us
 	* when we open a file in mcisendstring we can easily play pause or resume, but first we need the string command to open it
-	* to do that it uses this layout "open path mpegvideo alias name" 
-	* the open at the start says we want to open this file which is a 
+	* to do that it uses this layout "open path mpegvideo alias name"
+	* the open at the start says we want to open this file which is a
 	* the path shows us where that file is on the computer
 	* mpegvideo is the type of file
 	* alias can be anything it is just a name so we know what this opened file is classified as, can be mp3 wav or as I choose song
+	* more stuff on the open function of mcisendtstring  https://learn.microsoft.com/en-us/windows/win32/multimedia/open
 	* -Mazen
 	*/
 
@@ -325,9 +472,16 @@ string mcicommand(string path) {
 
 
 int main() {
+
+	/*__________________________________________________ QUOTE UN QUOTE __________________________________________________*/
+
+
+
+
+
 	/*------------------------------------------ APETIZERS ------------------------------------------*/
 
-	int controller; 
+	int controller;
 	string command;
 	time_t t1{}, t2{}, previous_pause_time = 0; //this is a timer saver for when we pause a song
 
@@ -337,8 +491,8 @@ int main() {
 
 	/*_______________________________ LIST SONGS __________________________________*/
 
-	vector<string> playlist; //the playlist including all the paths to the songs we use
-	vector<string> itemlist; //the itemlist which includes all the song names we use
+	fywauv::vector playlist;  //the playlist including all the paths to the songs we use
+	fywauv::vector itemlist; //the itemlist which includes all the song names we use
 	string ogpath;
 	cout << "\n\nselect your music folder using path: ";
 	cin >> ogpath; //asks for the first path to the starting folder
@@ -358,32 +512,32 @@ int main() {
 
 
 	cout << "Output of vector:\n\n";
-	for (int i = 0; i < playlist.size(); ++i) {
-		cout << playlist[i] << " " << endl;
+	for (int i = 0; i < playlist.getsize(); ++i) {
+		cout << playlist.get(i) << " " << endl;
 	}
 
-	cout << "AND SIZE IS..." << playlist.size() << endl;
+	cout << "AND SIZE IS..." << playlist.getsize() << endl;
 
 	/*______________________________CHOOSE SONG___________________________*/
 
 
 	/*
-	* this is the function for choosing which song to play first from the given playlist at the start 
+	* this is the function for choosing which song to play first from the given playlist at the start
 	*/
 
 
-	int songnum; 
+	int songnum;
 	cout << "\n\nenter song number: ";
 	cin >> songnum;
 
-	if (songnum > playlist.size()) {
+	if (songnum > playlist.getsize()) {
 		cout << "song number not found, track 1 chosen";
 		songnum = 1;
 	}
 
 	songnum = songnum - 1;
 
-	string song = playlist[songnum];
+	string song = playlist.get(songnum);
 
 
 	cout << "YOU CHOSE :   " << TEXT(song.c_str()) << endl;
@@ -404,7 +558,7 @@ int main() {
 	* the main controls of the program, first it initiliazes the mcisendstring with the command from the last function to open that song
 	*/
 
-	mciSendString(command.c_str(), NULL, 0, NULL); 
+	mciSendString(command.c_str(), NULL, 0, NULL);
 
 
 	while (true) {
@@ -427,7 +581,7 @@ int main() {
 		case 2: //stop
 
 			//because of my (Mazen) low knowledge of the mcisendstring function me and Ahmed decided we will making the stop function like this
-			
+
 			mciSendString("close song", NULL, 0, NULL); //closes the song
 			mciSendString(command.c_str(), NULL, 0, NULL); //opens it from the start with the command function
 			break;
@@ -435,7 +589,7 @@ int main() {
 
 			//uses the mcisendstring command pause to pause the song
 
-			mciSendString("pause song", NULL, 0, NULL); 
+			mciSendString("pause song", NULL, 0, NULL);
 			t2 = time(nullptr);
 			previous_pause_time += t2 - t1;
 			break;
@@ -443,23 +597,23 @@ int main() {
 
 			//uses the mcisendstring command resume to resume the song
 
-			mciSendString("resume song", NULL, 0, NULL); 
+			mciSendString("resume song", NULL, 0, NULL);
 			t1 = time(nullptr);
 			break;
 		case 5: //next
 
 			/*
 			* this is more complicated than just using a next function in mcisendstring, since when you use it, it works with one song at a time
-			* so Ahmed made this using the previous value songnum to be able to switch tracks 
+			* so Ahmed made this using the previous value songnum to be able to switch tracks
 			* -Mazen
 			*/
 
 			mciSendString("close song", NULL, 0, NULL); //first it closes the song we are playing right now
 			songnum++; //second it changes the songnum value increasing it by one 
-			if (songnum > playlist.size() - 1) { //this is so if the number is out of the playlist, it starts the playlist from the begining
+			if (songnum > playlist.getsize() - 1) { //this is so if the number is out of the playlist, it starts the playlist from the begining
 				songnum = 0;
 			}
-			song = playlist[songnum]; //this changes song which is a value holding a path from the old song to the path of the new song
+			song = playlist.get(songnum); //this changes song which is a value holding a path from the old song to the path of the new song
 			command = mcicommand(song); //send back the new path to the function command to make us the new command to use in mcisend string
 			mciSendString(command.c_str(), NULL, 0, NULL); //opens the new song using the command made in the previous line
 			mciSendString("play song", NULL, 0, NULL); //starts playing the new song
@@ -471,9 +625,9 @@ int main() {
 			mciSendString("close song", NULL, 0, NULL);
 			songnum--;
 			if (songnum < 0) {
-				songnum = playlist.size() - 1;
+				songnum = playlist.getsize() - 1;
 			}
-			song = playlist[songnum];
+			song = playlist.get(songnum);
 			command = mcicommand(song);
 			mciSendString(command.c_str(), NULL, 0, NULL);
 			mciSendString("play song", NULL, 0, NULL);
@@ -489,8 +643,8 @@ int main() {
 
 			//read the titel :D
 
-			for (int i = 0; i < itemlist.size(); i++) {
-				cout << i + 1 << "- " << itemlist[i] << endl;
+			for (int i = 0; i < itemlist.getsize(); i++) {
+				cout << i + 1 << "- " << itemlist.get(i) << endl;
 			}
 			break;
 
